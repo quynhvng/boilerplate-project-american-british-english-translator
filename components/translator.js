34710f4dd926216afddfaa28af114f1d0c2b026d
Quỳnh Vương <43884073+quynhvng@users.noreply.github.com>
2023-09-15 17:45:59 +0700

    Update translator.js

--- components/translator.js
+++ components/translator.js
@@ -6,82 +6,79 @@ const britishToAmericanSpelling = reverseObj(americanToBritishSpelling);
 const britishToAmericanTitles = reverseObj(americanToBritishTitles);
 
 const dicts = {
-  a2b: {
-    unique: americanOnly, 
-    title: americanToBritishTitles,
-    spelling: americanToBritishSpelling,
-    timeformat: {
-      regex: new RegExp('(\\d{1,2}):(\\d{1,2})', 'g'),
-      to: '.' 
+    a2b: {
+        unique: americanOnly,
+        title: americanToBritishTitles,
+        spelling: americanToBritishSpelling,
+        timeformat: {
+            regex: new RegExp('(\\d{1,2}):(\\d{1,2})', 'g'),
+            to: '.'
+        }
+    },
+    b2a: {
+        unique: britishOnly,
+        title: britishToAmericanTitles,
+        spelling: britishToAmericanSpelling,
+        timeformat: {
+            regex: new RegExp('(\\d{1,2})\\.(\\d{1,2})', 'g'),
+            to: ':'
+        }
     }
-  },
-  b2a: {
-    unique: britishOnly,
-    title: britishToAmericanTitles,
-    spelling: britishToAmericanSpelling,
-    timeformat: {
-      regex: new RegExp('(\\d{1,2})\\.(\\d{1,2})', 'g'),
-      to: ':' 
-    }
-  }
 }
 
 function reverseObj(obj) {
-  let res = {};
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      res[obj[key]] = key;
+    let res = {};
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            res[obj[key]] = key;
+        }
     }
-  }
-  return res;
+    return res;
 }
 
 function highlight(str) {
-  if (!str || str == '') return str;
-  return `<span class="highlight">${str}</span>`;
+    if (!str || str == '') return str;
+    return `<span class="highlight">${str}</span>`;
 }
 
 class Translator {
-  constructor(locale) {
-    this.locale = locale;
-    this.dict = dicts[locale];
-  }
+    constructor(locale) {
+        this.locale = locale;
+        this.dict = dicts[locale];
+    }
 
-  translate(str) {
-    //console.time('test');
-    let res = str.trim();
-    const lowerCase = res.toLowerCase();
-    // translated unique words
-    for (const w in this.dict.unique) {
-      if (lowerCase.includes(w)) {
-        res = res.replace(new RegExp(`\\b${w}\\b`, 'gi'), highlight(this.dict.unique[w]));
-      }
-    }
-    // translated titles
-    for (const w in this.dict.title) {
-      if (lowerCase.includes(w)) {
-        let regexStr = `\\b${w}\\b`;
-        if (this.locale = 'a2b') {
-          regexStr = regexStr.replace('.\\b', '\\.');
+    translate(str) {
+        let res = str.trim();
+        const lowerCase = res.toLowerCase();
+        
+        // convert title
+        let words = res.split(' ').map(w => {
+            let translated = this.dict.title[w.toLowerCase()];
+            return (translated) ? highlight(translated[0].toUpperCase()+translated.slice(1)) : w;
+        })
+        res = words.join(' ');
+      
+        // convert spelling
+        words = res.split(/\b/).map(w => {
+            let translated = this.dict.spelling[w.toLowerCase()];
+            return (translated) ? highlight(translated) : w;
+        })
+        res = words.join('');
+        
+        // convert time format
+        res = res.replace(this.dict.timeformat.regex, (match, p1, p2) => {
+            return highlight([p1, this.dict.timeformat.to, p2].join(''));
+        });
+
+        // translated unique words
+        for (const w in this.dict.unique) {
+          if (lowerCase.includes(w)) {
+            res = res.replace(new RegExp(`\\b${w}\\b`, 'gi'), highlight(this.dict.unique[w]));
+          }
         }
-        const [first, ...rest] = [...this.dict.title[w]];
-        const translated = [first.toUpperCase(), ...rest].join('');
-        res = res.replace(new RegExp(regexStr, 'gi'), highlight(translated));
-      }
+        
+        return res;
     }
-    // convert spelling
-    let words = res.split(/\b/).map(w => {
-      const translated = this.dict.spelling[w.toLowerCase()];
-      return (translated) ? highlight(translated) : w;
-    })
-    res = words.join('');
-    // convert time format
-    res = res.replace(this.dict.timeformat.regex, (match, p1, p2) => {
-      return highlight([p1, this.dict.timeformat.to, p2].join(''));
-    });
-    //console.timeEnd('test');
-    return res;
-  }
 }
 
 module.exports = Translator;
